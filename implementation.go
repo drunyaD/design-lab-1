1a316dc2f8b0348cf8d1021e476a9c9af7f97171
andrey_shelkovoy1 andrey_shelkovoy1 <drunyashel@gmail.com>
2020-02-22 19:02:26 +0200

    added validation while parsing

--- implementation.go
+++ implementation.go
@@ -1,9 +1,41 @@
 package lab1
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func contains(s []string, e string) bool {
+    for _, a := range s {
+        if a == e {
+            return true
+        }
+    }
+    return false
+}
+
+func isNumeric(s string) bool {
+    _, err := strconv.ParseInt(s, 10, 64)
+    return err == nil
+}
 
 // TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
+// Prefix form calculates
+func PrefixCalculate(input string) (string	, error) {
+	if input == "" {
+		return "", fmt.Errorf("input should not be empty")
+	}
+
+	//res := 0
+	splitedString := strings.Split(input, " ")
+	signs := []string{"+", "-", "*", "/", "^"}
+
+	for _, symbol := range splitedString {
+		if (contains(signs, symbol) == false && isNumeric(symbol) == false) {
+			return "", fmt.Errorf("failed on parsing : invalid input")
+		}
+		
+	}
 	return "TODO", fmt.Errorf("TODO")
 }
